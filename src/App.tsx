e29593da000f75d9d204aba1ea91303a704c474e
PedroGont <pedrotolentino2002@gmail.com>
2022-03-07 23:14:42 -0300

    FormularioQueSalvaDadosEmBD

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FormSubmit from "./Components/formSubmit"
+
+function App() {
+  return (
+    <div>
+      <FormSubmit/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/formSubmit.tsx
@@ -0,0 +1,55 @@
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import api from '../services/api';
+
+type DataProps = {
+  name: string,
+  email: string,
+}
+
+function FormSubmit() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  function handleSubmit(){
+    const data = {name: name, email: email}
+    
+    const response = api.post('/users', data)
+
+    console.log(response)
+  }
+
+  return (
+    <div>
+      <Form onSubmit={(event: any) => {
+        event.preventDefault()
+        handleSubmit()
+      }}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" value={name} onChange={(event) =>{
+            setName(event.target.value)
+          }} placeholder="Enter name" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" value={email} onChange={(event) =>{
+            setEmail(event.target.value)
+          }} placeholder="Email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default FormSubmit
